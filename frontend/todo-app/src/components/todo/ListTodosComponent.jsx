50bbd0b865bbfef5b1d0e5e3a6653ff2cdc94cff
Sandhi Sayem <Sandhi.Sayem@Gmail.com>
2024-03-01 11:10:56 -0500

    Chapter 12, Step 11

--- frontend/todo-app/src/components/todo/ListTodosComponent.jsx
+++ frontend/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,34 +1,54 @@
-function ListTodoComponent() {
-    const today = new Date();
-    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay());
+import { useEffect, useState } from "react";
+import { retrieveAllTodosForUsernameApi, deleteTodoApi } from "./api/TodoApiService";
+import { useAuth } from "./security/AuthContext";
 
-    const todos = [
-        { id: 1, description: "Learn AWS", done: false, targetDate: targetDate },
-        { id: 2, description: "Learn Full Stack Dev", done: false, targetDate: targetDate },
-        { id: 3, description: "Learn DevOps", done: false, targetDate: targetDate },
-        { id: 4, description: "Learn Azure", done: false, targetDate: targetDate },
-    ];
+function ListTodoComponent() {
+
+    const authContext = useAuth();
+
+    const [todos, setTodos] = useState([]);
+    const [message, setMessage] = useState(null);
+
+    useEffect(() => refreshTodos(), [])
+
+    const refreshTodos = () => {
+        retrieveAllTodosForUsernameApi(authContext.username)
+            .then(response => setTodos(response.data))
+            .catch(error => console.log(error))
+    }
+
+    const deleteTodo = (id) => {
+        console.log(`clicked ${id}`)
+        deleteTodoApi(authContext.username, id)
+            .then(() => {
+                setMessage(`Delete of todo with id = ${id} successful`)
+                refreshTodos()
+            })
+            .catch(error => console.log(error))
+    }
 
     return (
         <div className="container">
             <h1>Things You Want To Do!</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div>
                 <table className="table">
                     <thead>
                         <tr>
-                            <td>ID</td>
-                            <td>Description</td>
-                            <td>is done?</td>
-                            <td>Target Date</td>
+                            <th>Description</th>
+                            <th>is done?</th>
+                            <th>Target Date</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
                         {todos.map(todo => (
                             <tr key={todo.id}>
-                                <td>{todo.id}</td>
                                 <td>{todo.description}</td>
                                 <td>{todo.done.toString()}</td>
-                                <td>{todo.targetDate.toDateString()}</td>
+                                {/* <td>{todo.targetDate.toDateString()}</td> */}
+                                <td>{todo.targetDate.toString()}</td>
+                                <td><button className="btn btn-warning" onClick={() => deleteTodo(todo.id)}>Delete</button></td>
                             </tr>
                         ))}
                     </tbody>
--- frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,12 +1,48 @@
 import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
+import { retrieveHelloWorldPathVariable } from "./api/HelloWorldApiService";
 
 function WelcomeComponent() {
     const { username } = useParams();
+
+    const [message, setMessage] = useState(null);
+
+    const callHelloWorldRestApi = () => {
+
+
+        // axios.get("http://localhost:8080/hello-world")
+        //     .then(response => succesfulResponse(response))
+        //     .catch(error => errorResponse(error))
+        //     .finally(() => console.log('cleanup'))
+
+        // retrieveHelloWorldBean()
+        //     .then(response => succesfulResponse(response))
+        //     .catch(error => errorResponse(error))
+        //     .finally(() => console.log('cleanup'))
+
+        retrieveHelloWorldPathVariable('Sayem')
+            .then(response => succesfulResponse(response))
+            .catch(error => errorResponse(error))
+            .finally(() => console.log('cleanup'))
+    }
+
+    const succesfulResponse = response => {
+        console.log(response);
+        setMessage(response.data.message);
+    }
+
+    const errorResponse = error => {
+        console.log(error);
+    }
 
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username}!</h1>
             <div>Manage your todos - <Link to="/todos">Go here</Link></div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     );
 }
