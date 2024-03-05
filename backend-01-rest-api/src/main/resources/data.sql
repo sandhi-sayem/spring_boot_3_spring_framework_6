e0d0e0dc8e46ba0b93be0b68561829dc0af03dec
Sandhi Sayem <Sandhi.Sayem@Gmail.com>
2024-03-04 21:04:34 -0500

    Chapter 13, step 5 - chapter end

---
+++ backend-01-rest-api/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (id, description, done, target_date, username)
+values (10001, 'Learn AWS', false, current_date(), 'sayem');
+
+insert into todo (id, description, done, target_date, username)
+values (10002, 'Get AWS Certifiied', false, current_date(), 'sayem');
+
+insert into todo (id, description, done, target_date, username)
+values (10003, 'Learn DevOps', false, current_date(), 'sayem');
+
+insert into todo (id, description, done, target_date, username)
+values (10004, 'Learn GCP', false, current_date(), 'sayem');
